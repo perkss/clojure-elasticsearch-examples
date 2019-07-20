64d6f04302813237d60b8e2fa0fa9660983bf926
perkss <stuart.m.perks@gmail.com>
2019-07-20 19:01:02 +0100

    Initial commit

---
+++ src/clojure_elasticsearch_examples/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-elasticsearch-examples.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojure_elasticsearch_examples/core_test.clj
@@ -0,0 +1,3 @@
+(ns clojure-elasticsearch-examples.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-elasticsearch-examples.core :refer :all]))
