6e8adf4c1e6f1ea6fea7f1d44885f8009be32e6f
perkss <stuart.m.perks@gmail.com>
2019-07-22 20:17:13 +0100

    Initial index creation example

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject clojure-elasticsearch-examples "0.1.0-SNAPSHOT"
   :description "Elastic search with Clojure fun"
   :url "perks.github.io"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[io.searchbox/jest "5.3.3"]
+                 [org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
+                 [org.elasticsearch/elasticsearch "7.2.0"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
